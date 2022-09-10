bbc65a1cfd7077880d245b3592f07df975f5330c
Affaan Kidwai <kidwaiaffaan@gmail.com>
2022-09-11 01:49:47 +0530

    coin symbols added

---
+++ src/components/CoinSearch.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import {AiOutlineStar} from 'react-icons/ai'
+
+const CoinSearch = ({coins}) => {
+    // console.log(coins);
+  return (
+    <div>
+        <div>
+            <h1>Search Crypto</h1>
+            <form>
+                <input type="text" placeholder='Search a coin'/>
+            </form>
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>#</th>
+                    <th>Coin</th>
+                    <th></th>
+                    <th>Price</th>
+                    <th>24h</th>
+                    <th>24 Volume</th>
+                    <th>Mkt</th>
+                    <th>Last 7 Days</th>
+                </tr>
+            </thead>
+            <tbody>
+                {coins.map((coin) => (
+                    <tr>
+                        <td><AiOutlineStar /></td>
+                        <td>{coin.market_cap_rank}</td>
+                        <td><div>
+                            <img src={coin.image} alt={coin.id} />
+                            <p>{coin.name}</p>
+                            </div></td>
+                        <td>{coin.symbol}</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                    </tr>
+                ))}
+            </tbody>
+            </table>
+    </div>
+  )
+}
+
+export default CoinSearch
