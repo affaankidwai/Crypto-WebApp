47a249bf1e73418684ff6f56dcb24c90cc5d28b4
Affaan Kidwai <kidwaiaffaan@gmail.com>
2022-09-11 00:37:21 +0530

    dark light toggle added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { ThemeProvider } from './context/ThemeContext'
 
 const App = () => {
   return (
-    <div>
+    <ThemeProvider>
       <Navbar />
-    </div>
+    </ThemeProvider>
   )
 }
 
