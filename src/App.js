6c987c8dda69463da6685c094a6d82aa8a243556
Affaan Kidwai <kidwaiaffaan@gmail.com>
2022-09-11 03:01:05 +0530

    Coins are displayed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import axios from 'axios'
 
 const App = () => {
   const [coins, setCoins] = useState([])
-  const url ="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+  const url ="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true"
 
   useEffect(() => {
     axios.get(url).then((response) => {
